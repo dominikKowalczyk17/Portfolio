1f1716694bff43eadd381b913ccb883da3f54bef
dkowalczyk <d.kowalczyk@dituel.pl>
2023-12-12 16:09:25 +0100

    add about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout/Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import './App.scss'
+import About from './components/About/About.jsx'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
